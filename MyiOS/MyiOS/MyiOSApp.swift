d1c21ac885a623031729fe5c2aa76b14bf65cffa
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-02-19 22:59:46 +0300

    Add files via upload

---
+++ MyiOS/MyiOS/MyiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyiOSApp.swift
+//  MyiOS
+//
+//  Created by Engin Gülek on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
